170723b7a4c2785d8d222ce34676f090dea7d3c1
lalurama <lalurama26@gmail.com>
2026-01-03 01:42:13 +0800

    update typo

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\StockController;
+use App\Http\Controllers\api\StockController;
 
 // Mock API endpoint - sesuai format original
 Route::get('/product/stock/check', [StockController::class, 'check']);
